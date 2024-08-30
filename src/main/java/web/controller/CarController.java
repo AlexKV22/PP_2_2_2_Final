f5a35c0fa635d8896284dcffc77f9d933c727ef2
aleksandrkakovkin <sasha22.94@mail.ru>
2024-08-30 14:17:44 +0300

    Change the CarController

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,10 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.ServiceCar;
 import web.service.ServiceCarImpl;
 
 import java.util.ArrayList;
@@ -12,17 +14,18 @@ import java.util.List;
 
 @Controller
 public class CarController {
+    @Autowired
+    private ServiceCar serviceCar;
+
+    public CarController(ServiceCarImpl serviceCar) {
+        this.serviceCar = serviceCar;
+    }
+
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(value = "count", defaultValue = "5") int sizeCar, ModelMap model) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car(" Mazda1", 2000, 55000 ));
-        cars.add(new Car(" Mazda2", 2001, 56000 ));
-        cars.add(new Car(" Mazda3", 2002, 57000 ));
-        cars.add(new Car(" Mazda4", 2003, 58000 ));
-        cars.add(new Car(" Mazda5", 2004, 59000 ));
-        cars = new ServiceCarImpl().getCars(cars, sizeCar);
-        model.addAttribute("cars", cars);
+        List<Car> cars1 = serviceCar.getCars(sizeCar);
+        model.addAttribute("cars", cars1);
         return "carscars";
     }
 }
--- src/main/java/web/service/ServiceCarImpl.java
+++ src/main/java/web/service/ServiceCarImpl.java
@@ -1,12 +1,23 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class ServiceCarImpl implements ServiceCar {
-    public  List<Car> getCars(List<Car> cars, int count) {
+
+
+    public  List<Car> getCars(int count) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car(" Mazda1", 2000, 55000 ));
+        cars.add(new Car(" Mazda2", 2001, 56000 ));
+        cars.add(new Car(" Mazda3", 2002, 57000 ));
+        cars.add(new Car(" Mazda4", 2003, 58000 ));
+        cars.add(new Car(" Mazda5", 2004, 59000 ));
         if (count >= cars.size()) {
             return cars;
         }
